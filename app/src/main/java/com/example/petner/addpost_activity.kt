29faffde9d7873f8e6d530dabcb8d19daf500897
kimjehyeon <dlsrks0631@naver.com>
2023-11-23 01:10:35 +0900

    create post screen

--- app/src/main/java/com/example/petner/addpost_activity.kt
+++ app/src/main/java/com/example/petner/addpost_activity.kt
@@ -3,9 +3,9 @@ package com.example.petner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class addpost_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_post)
     }
 }
